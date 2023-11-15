6f57c838189cec5b0bf241392fce17b9696e962d
RamazanImashov <ramazanimashov@gmil.com>
2023-11-15 23:56:24 +0600

    chat

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     "corsheaders",
 
     "account",
-
+    'channels',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
 
 
 # Database
@@ -134,9 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
